8f8b3d948f13bf200404ecefd914465542af2c54
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-15 14:51:28 +0100

    fetch pending commit

--- src/app.js
+++ src/app.js
@@ -2,9 +2,24 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue ({
-    el: "#app",
+    el: '#app',
     data: {
-      header: "Hello"
+      header: "Countries",
+      countries: [
+        {name: "", population: ""}
+      ]
     },
-  });
-});
+    mounted(){
+      this.fetchPopulation();
+    },
+    methods: {
+      fetchPopulation: function () {
+        const request = fetch("https://restcountries.eu/rest/v2/all");
+        console.log(request)
+        // .then(response => response.json())
+        // .then(data => this.countries.name = data.message);
+      }
+    }
+
+  })
+})
